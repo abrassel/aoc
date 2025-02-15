db198c15174f4ba6b339242ae7605d85b80bee97
Alexander Brassel <awildbrasselsprouts@gmail.com>
2025-02-15 08:14:14 -0800

    day10p2

--- src/maze.rs
+++ src/maze.rs
@@ -1,10 +1,12 @@
-use std::convert::Infallible;
+use std::{collections::HashSet, convert::Infallible, ops::Index};
 
 use crate::utls::MyParse;
 
-#[derive(PartialEq, Clone)]
+#[derive(PartialEq, Clone, derive_more::Display)]
 pub enum Cell {
+    #[display("#")]
     Wall,
+    #[display(".")]
     Open,
 }
 
@@ -23,6 +25,29 @@ impl TryFrom<char> for Cell {
 #[derive(Clone)]
 pub struct Maze {
     pub grid: Vec<Vec<Cell>>,
+}
+
+impl Maze {
+    pub fn print_with_destroyed(&self, start: (i32, i32), destroyed: &[(i32, i32)]) {
+        let destroyed: Vec<(usize, usize)> = destroyed
+            .iter()
+            .map(|coord| (coord.0 as usize, coord.1 as usize))
+            .collect();
+
+        for (ridx, row) in self.grid.iter().enumerate() {
+            for (cidx, cell) in row.iter().enumerate() {
+                if let Some(idx) = destroyed.iter().position(|l| &(cidx, ridx) == l) {
+                    print!("{}", idx % 10);
+                } else if (cidx, ridx) == (start.0 as usize, start.1 as usize) {
+                    print!("O");
+                } else {
+                    print!("{}", cell);
+                }
+            }
+            println!();
+        }
+        println!("finished printing");
+    }
 }
 
 impl MyParse for Maze {
--- src/year_2019/day_10.rs
+++ src/year_2019/day_10.rs
@@ -1,64 +1,148 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Display,
+    io::Write,
+};
 
 use fraction::GenericFraction;
 use itertools::Itertools;
 
 use crate::maze::{Cell, Maze};
 
-fn part_1(maze: Maze) -> usize {
+fn part_1(maze: &Maze) -> usize {
     let points = get_points(maze);
     let lines = lines(&points);
-    // print_points_on_lines(&lines);
-    let counts = counts(lines);
+    print_points_on_lines(&lines);
+    let counts = counts(&lines);
     *counts.values().max().unwrap()
 }
 
-fn part_2(maze: Maze) {}
-
-fn get_points(maze: Maze) -> Vec<(i32, i32)> {
-    maze.grid
+fn part_2(maze: &Maze, nth: usize) -> Point {
+    let lines = lines(&get_points(maze));
+    // only take lines which contain the best point
+    let counts = counts(&lines);
+    let best_point = counts.into_iter().max_by_key(|x| x.1).unwrap().0;
+    let relevant_lines = lines
         .into_iter()
+        .filter(|(_, points)| points.contains(&best_point));
+    // sort lines by maximum slope to minimum -> this simulates clockwise rotation (maximum incidence at y = (big_m) * x + b)
+    let inorder_lines = relevant_lines
+        .into_iter()
+        .sorted_unstable_by_key(|((slope, _), _)| *slope)
+        .rev();
+
+    let copy: Vec<(Line, _)> = inorder_lines
+        .clone()
+        .map(|(line, points)| (line.into(), points))
+        .collect_vec();
+    // partition each line by points left and right (or above and below for vertical) of laser location
+    let inorder_lines_with_y_axis_sep = inorder_lines
+        .map(|(_, points)| points)
+        .map(|points| -> (Vec<_>, Vec<_>) {
+            points.into_iter().partition(|point| point < &best_point)
+        })
+        .map(|(mut left, mut right)| {
+            // left side is origin -> end of line, so want to go in descending order
+            left.sort_unstable_by(|lft, rht| rht.cmp(lft));
+            right.sort_unstable();
+            right.remove(0);
+            (left, right)
+        });
+    let (lhs, rhs): (Vec<_>, Vec<_>) = inorder_lines_with_y_axis_sep.unzip();
+    let mut lhs: Vec<_> = lhs.into_iter().map(|line| line.into_iter()).collect();
+    let mut rhs: Vec<_> = rhs.into_iter().map(|line| line.into_iter()).collect();
+    let mut count = 0;
+    let mut destroyed = vec![];
+
+    maze.print_with_destroyed(best_point.into(), &destroyed);
+    for _ in 0..500 {
+        for line in rhs.iter_mut().chain(lhs.iter_mut()) {
+            if let Some(nxt) = line.next() {
+                count += 1;
+                destroyed.push(nxt.into());
+                maze.print_with_destroyed(best_point.into(), &destroyed);
+                if count == nth {
+                    return nxt;
+                }
+            }
+        }
+    }
+    panic!("Was not 200 points");
+}
+
+fn get_points(maze: &Maze) -> Vec<Point> {
+    maze.grid
+        .iter()
         .enumerate()
         .flat_map(|(ridx, row)| {
-            row.into_iter()
+            row.iter()
                 .enumerate()
-                .filter(|(_, cell)| cell == &Cell::Wall)
-                .map(move |(cidx, _)| (ridx.try_into().unwrap(), cidx.try_into().unwrap()))
+                .filter(|(_, cell)| **cell == Cell::Wall)
+                .map(move |(cidx, _)| (cidx.try_into().unwrap(), ridx.try_into().unwrap()).into())
         })
         .collect()
 }
 
-#[derive(Eq, PartialEq, Hash, Debug)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Ord, PartialOrd, derive_more::Display)]
 enum Slope {
-    Horizontal,
+    #[display("{}/{}", _0.numer().unwrap(), _0.denom().unwrap())]
     Normal(GenericFraction<i32>),
     Vertical,
 }
 
-type PointsOnLines = HashMap<Slope, HashMap<GenericFraction<i32>, HashSet<(i32, i32)>>>;
+type PointsOnLines = HashMap<(Slope, GenericFraction<i32>), HashSet<Point>>;
+#[derive(Ord, PartialOrd, PartialEq, Eq, Hash, Clone, Copy, derive_more::Display)]
+#[display("({}, {})", _0, -_1)]
+struct Point(i32, i32);
+impl std::fmt::Debug for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        <Point as Display>::fmt(self, f)
+    }
+}
 
-// fn print_points_on_lines(points_on_lines: &PointsOnLines) {
-//     for (slope, offsets) in points_on_lines {
-//         for (offset, points) in offsets {
-//             println!(
-//                 "Line y = {:?}x + {} contains these points: {:?}",
-//                 slope, offset, points
-//             );
-//         }
-//     }
-// }
+impl From<Point> for (i32, i32) {
+    fn from(val: Point) -> Self {
+        (val.0, -val.1)
+    }
+}
 
-fn lines(points: &[(i32, i32)]) -> PointsOnLines {
+impl From<(i32, i32)> for Point {
+    fn from(value: (i32, i32)) -> Self {
+        Self(value.0, -value.1)
+    }
+}
+
+#[derive(derive_more::From)]
+struct Line(Slope, GenericFraction<i32>);
+
+impl std::fmt::Debug for Line {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "y={}x+{}", self.0, self.1)
+    }
+}
+
+fn print_points_on_lines(points_on_lines: &PointsOnLines) {
+    for (slope, points) in points_on_lines {
+        (
+            "Line {:?} contains these points: {:?}",
+            Line::from(*slope),
+            points,
+        );
+    }
+}
+
+fn lines(points: &[Point]) -> PointsOnLines {
     let mut lines: PointsOnLines = HashMap::new();
     for combination in points.iter().combinations(2) {
         let f = combination[0];
         let t = combination[1];
         let slope = {
             if t.1 - f.1 == 0 {
-                Slope::Horizontal
+                Slope::Normal(GenericFraction::from(0))
             } else if t.0 - f.0 == 0 {
                 Slope::Vertical
             } else {
+                // the gridspace is y-flipped
                 let slope = ((t.1 - f.1), (t.0 - f.0));
                 let div = num::integer::gcd(slope.0, slope.1);
                 let slope = GenericFraction::new(slope.0 / div, slope.1 / div);
@@ -66,20 +150,19 @@ fn lines(points: &[(i32, i32)]) -> PointsOnLines {
             }
         };
         let b = match slope {
-            Slope::Horizontal => t.1.into(),
             Slope::Normal(slope) => GenericFraction::from(t.1) - slope * t.0,
             Slope::Vertical => t.0.into(),
         };
-        let line_bucket = lines.entry(slope).or_default().entry(b).or_default();
+        let line_bucket = lines.entry((slope, b)).or_default();
         line_bucket.insert(*f);
         line_bucket.insert(*t);
     }
     lines
 }
 
-fn counts(points_on_lines: PointsOnLines) -> HashMap<(i32, i32), usize> {
+fn counts(points_on_lines: &PointsOnLines) -> HashMap<Point, usize> {
     let mut counts = HashMap::new();
-    for points in points_on_lines.values().flat_map(|line| line.values()) {
+    for points in points_on_lines.values() {
         for &point in points {
             // each neighbor is visible, or else there would be another point in the way
             *counts.entry(point).or_default() += 2;
@@ -97,9 +180,9 @@ fn counts(points_on_lines: PointsOnLines) -> HashMap<(i32, i32), usize> {
 pub fn run() {
     let maze: Maze = crate::utls::read_text_from_file("2019", "10");
 
-    let res = part_1(maze.clone());
-    println!("The answer is {}", res);
-    part_2(maze);
+    let res = part_2(&maze.clone(), 200);
+    let points: (i32, i32) = res.into();
+    println!("The answer is {}", points.0 * 100 + points.1);
 }
 
 #[cfg(test)]
@@ -109,7 +192,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_solution() {
+    fn test_solution_base() {
         let input = r"
         .#..#
 .....
@@ -118,7 +201,7 @@ mod test {
 ...##
 ";
         let maze = Maze::my_parse(input);
-        let ans = part_1(maze);
+        let ans = part_1(&maze);
         assert_eq!(ans, 8);
     }
 
@@ -136,7 +219,7 @@ mod test {
 ##...#..#.
 .#....####";
         let maze = Maze::my_parse(input);
-        let ans = part_1(maze);
+        let ans = part_1(&maze);
         assert_eq!(ans, 33);
     }
 
@@ -154,7 +237,7 @@ mod test {
 ......#...
 .####.###.";
         let maze = Maze::my_parse(input);
-        let ans = part_1(maze);
+        let ans = part_1(&maze);
         assert_eq!(ans, 35);
     }
 
@@ -172,7 +255,7 @@ mod test {
 .##...##.#
 .....#.#..";
         let maze = Maze::my_parse(input);
-        let ans = part_1(maze);
+        let ans = part_1(&maze);
         assert_eq!(ans, 41);
     }
 
@@ -200,7 +283,54 @@ mod test {
 #.#.#.#####.####.###
 ###.##.####.##.#..##";
         let maze = Maze::my_parse(input);
-        let ans = part_1(maze);
+        let ans = part_1(&maze);
         assert_eq!(ans, 210);
+    }
+
+    #[test]
+    fn test_solution_5() {
+        let input = r".#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##";
+        let maze = Maze::my_parse(input);
+        assert_eq!(part_2(&maze, 1), (11, 12).into());
+        assert_eq!(part_2(&maze, 2), (12, 1).into());
+        assert_eq!(part_2(&maze, 3), (12, 2).into());
+        assert_eq!(part_2(&maze, 10), (12, 8).into());
+        assert_eq!(part_2(&maze, 20), (16, 0).into());
+        assert_eq!(part_2(&maze, 50), (16, 9).into());
+        assert_eq!(part_2(&maze, 100), (10, 16).into());
+        assert_eq!(part_2(&maze, 199), (9, 6).into());
+        assert_eq!(part_2(&maze, 200), (8, 2).into());
+        assert_eq!(part_2(&maze, 201), (10, 9).into());
+        assert_eq!(part_2(&maze, 299), (11, 1).into());
+    }
+
+    #[test]
+    fn test_solution_6() {
+        let input = r".#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....#...###..
+..#.#.....#....##";
+        let maze = Maze::my_parse(input);
+        part_2(&maze, 20);
     }
 }
